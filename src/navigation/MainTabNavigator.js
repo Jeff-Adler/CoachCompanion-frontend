65ca7e25f23d2b38d2327f6c045d7977999b4796
Jeff-Adler <Jeffm.adler@gmail.com>
2020-11-08 11:13:17 -0500

    setup tab navigator

---
+++ src/navigation/MainTabNavigator.js
@@ -0,0 +1,37 @@
+import React from "react"
+
+import {creatBottomTabNavigator} from "@react-navigation/bottom-tabs"
+
+const Tab = createBottomTabNavigator();
+
+const MainTabNavigator = (props) => {
+    const {currentUser} = props
+
+    return (
+        <Tab.Navigator
+            initialRouteName = "Log Activity"
+            // screenOptions={({ route }) => ({
+            //     tabBarIcon: ({ focused, color, size }) => {
+            //     let iconName;
+            //     if (route.name === "Posts") {
+            //         iconName = focused ? "comments" : "comments";
+            //     } else if (route.name === "Account") {
+            //         iconName = focused ? "user-alt" : "user";
+            //     }
+            //     return <FontAwesome5 name={iconName} size={size} color={color} />;
+            //     },
+            // })}
+            // tabBarOptions={{
+            //     activeTintColor: "tomato",
+            //     inactiveTintColor: "gray",
+            // }}
+        >
+            <Tab.Screen name="Log Activity"/>
+            <Tab.Screen name="Receive Coaching" />
+            <Tab.Screen name="Input Activity" />
+            <Tab.Screen name="Profile" />
+        </Tab.Navigator>
+    )
+}
+
+export default MainTabNavigator;
