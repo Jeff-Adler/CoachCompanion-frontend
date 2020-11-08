382f50e0a467ae15d0159934719e82ecd53bd626
Jeff-Adler <Jeffm.adler@gmail.com>
2020-11-08 10:26:28 -0500

    setup basic file structure

--- App.js
+++ App.js
@@ -1,15 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+//Navigation Imports
+import { NavigationContainer } from "@react-navigation/native";
+import MainTabNavigator from "./src/navigation/MainTabNavigator";
+import LoginStackNavigator from "./src/navigation/LoginStackNavigator";
+
+class App extends React.Component {
+  state = {
+    user: null,
+    isSignedIn: false
+  }
+
+  render () {
+    const {isSignedIn , user} = this.state
+    return (
+      <View style = {styles.container}>
+        {
+          isSignedIn === true ? (
+            <View>
+              <NavigationContainer>
+                <MainTabNavigator
+                  currentUser={user}
+                  logoutHandler={this.logoutHandler}
+                />
+              </NavigationContainer>
+            </View>
+          ) : (
+            <View>
+              <NavigationContainer>
+                <LoginStackNavigator
+                  loginHandler={this.loginHandler}
+                  signupHandler={this.signupHandler}
+                />
+              </NavigationContainer>
+            </View>
+          )
+        }
+      </View>
+    )
+  }
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
