2d33e711aa530870dc1cc599751b8e7620979b8f
Jeff-Adler <Jeffm.adler@gmail.com>
2020-11-15 10:49:00 -0500

    get request

--- src/containers/LoggerContainer.js
+++ src/containers/LoggerContainer.js
@@ -5,18 +5,7 @@ class LoggerContainer extends React.Component {
 
     //input POST request to input new log
 
-    // create_table "activities", force: :cascade do |t|
-        // t.bigint "user_id", null: false
-        // t.string "title"
-        // t.integer "point_value"
-        // t.string "category"
-        // t.boolean "audible"
-        // t.string "energy_type"
-        // t.datetime "created_at", precision: 6, null: false
-        // t.datetime "updated_at", precision: 6, null: false
-        // t.index ["user_id"], name: "index_activities_on_user_id"
-    // end
-
+    //input GET request to get list of user's activities, to be rendered
 
     render() {
         return (
