da67ff2c3c3703c2dad8ab4f8afc070e3491821e
Jeff-Adler <Jeffm.adler@gmail.com>
2020-11-15 10:12:09 -0500

    logger skeleton

--- src/containers/LoggerContainer.js
+++ src/containers/LoggerContainer.js
@@ -5,6 +5,18 @@ class LoggerContainer extends React.Component {
 
     //input POST request to input new log
 
+    // create_table "activities", force: :cascade do |t|
+        // t.bigint "user_id", null: false
+        // t.string "title"
+        // t.integer "point_value"
+        // t.string "category"
+        // t.boolean "audible"
+        // t.string "energy_type"
+        // t.datetime "created_at", precision: 6, null: false
+        // t.datetime "updated_at", precision: 6, null: false
+        // t.index ["user_id"], name: "index_activities_on_user_id"
+    // end
+
     render() {
         return (
             <View style={styles.container}>
