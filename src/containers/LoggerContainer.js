0bd4c4ebd131cc056eade5904be31ab9ba678c5d
Jeff-Adler <Jeffm.adler@gmail.com>
2020-11-15 11:18:10 -0500

    added APi acll

--- src/containers/LoggerContainer.js
+++ src/containers/LoggerContainer.js
@@ -6,6 +6,30 @@ class LoggerContainer extends React.Component {
     //input POST request to input new log
 
     //input GET request to get list of user's activities, to be rendered
+    // 3000/activities
+
+    componentDidMount () {
+        const configObj = {
+            method: "POST",
+            headers: {
+              accepts: "application/json",
+              "content-type": "application/json",
+            },
+            body: JSON.stringify({ user: userInfo }),
+          };
+          
+        fetch("http://localhost:3000/api/v1/login", configObj)
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.jwt) {
+                this.storeToken(data.jwt);
+                this.setState({ user: data.user, isSignedIn: true });
+                } 
+                // else {
+                //   this.setState({ authenticationError: data.message });
+                // }
+            });
+    }
 
     render() {
         return (
