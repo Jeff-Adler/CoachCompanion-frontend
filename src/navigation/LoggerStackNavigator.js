9e45cccc7e769c2d2c5abadc69103452ebcdcaf1
Jeff-Adler <Jeffm.adler@gmail.com>
2020-11-16 13:10:40 -0500

    map activities

--- src/navigation/LoggerStackNavigator.js
+++ src/navigation/LoggerStackNavigator.js
@@ -2,13 +2,17 @@ import React from "react";
 import { StyleSheet } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 
+import ActivitySelection from '../screens/LoggerScreens/ActivitySelection'
+
 const LoggerStack = createStackNavigator()
 
 const LoggerStackNavigator = (props) => {
     return (
         <LoggerStack.Navigator style={styles.container} initialRouteName="Logger">
             <LoggerStack.Screen name="Logger">
-
+                <LoggerStack.Screen name="Login">
+                    {(props) => <ActivitySelection {...props}/>}
+                </LoggerStack.Screen>
             </LoggerStack.Screen>
         </LoggerStack.Navigator>
     )
--- src/screens/LoggerScreens/ActivitySelection.js
+++ src/screens/LoggerScreens/ActivitySelection.js
@@ -1,9 +1,31 @@
 import React from "react";
 
-import { StyleSheet } from "react-native";
+import { ListItem } from 'react-native-elements'
+
+import { View, ScrollView, StyleSheet } from "react-native";
 
 class ActivitySelection extends React.Component {
 
+    mapActivities = () => {
+        return this.props.activities.map((activity,i) => {
+            return (
+                <ListItem key={i} bottomDivider>
+                    <ListItem.Content>
+                        <ListItem.Title>{activity.title}</ListItem.Title>
+                        <ListItem.Subtitle>{activity.category}</ListItem.Subtitle>
+                    </ListItem.Content>
+                </ListItem>
+            )
+        })
+    }
+
+    render() {
+        return (
+            <View>
+                <ScrollView>{this.mapActivities()}</ScrollView>
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
