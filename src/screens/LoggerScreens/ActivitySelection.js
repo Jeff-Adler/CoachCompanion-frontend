cc0999e81b03f5e3e0a96704702d07f6c43faa9e
Jeff-Adler <Jeffm.adler@gmail.com>
2020-11-16 14:47:09 -0500

    got listview to show

--- src/screens/LoggerScreens/ActivitySelection.js
+++ src/screens/LoggerScreens/ActivitySelection.js
@@ -32,8 +32,8 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
+      alignItems: "stretch",
+    //   justifyContent: "center",
     },
   });
   
