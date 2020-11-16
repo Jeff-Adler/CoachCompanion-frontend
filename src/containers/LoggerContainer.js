173e15d7dbcda62f530eb2a870f9e156c7c1493c
Jeff-Adler <Jeffm.adler@gmail.com>
2020-11-16 09:29:27 -0500

    beggan activity selector

--- src/containers/LoggerContainer.js
+++ src/containers/LoggerContainer.js
@@ -1,26 +1,21 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
+import LoggerStackNavigator from "../navigation/LoggerStackNavigator";
 
 class LoggerContainer extends React.Component {
-
+    state = {activities: null}
     //input POST request to input new log
 
     //input GET request to get list of user's activities, to be rendered
     // 3000/activities
 
     componentDidMount () {
-        const token = await this.props.getToken();
+        // const token = await this.props.getToken();
+        const token = this.props.getToken();
         this.fetchActivities(token);
     }
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>LoggerContainer</Text>
-            </View>
-        )
-    }
-
+    
     fetchActivities = (token) => {
         const configObj = {
             method: "GET",
@@ -28,11 +23,22 @@ class LoggerContainer extends React.Component {
                 Authorization: `Bearer ${token}`,
             }
         }
-        fetch(`http://localhost:3000/api/v1/user/${userId}/activities`, configObj)
+        fetch(`http://localhost:3000/api/v1/user/${this.props.currentUser.id}/activities`, configObj)
             .then((response) => response.json())
             .then((data) => {
-               
+                this.setState({activities : data})
             });
+    }
+
+    render() {
+        const {activities} = this.state
+        return (
+            <View style={styles.container}>
+                <LoggerStackNavigator 
+                    activities={activities}
+                />
+            </View>
+        )
     }
 }
 
---
+++ src/screens/LoggerScreens/ActivitySelection.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { StyleSheet } from "react-native";
+
+class ActivitySelection extends React.Component {
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  });
+  
+  export default ActivitySelection;
