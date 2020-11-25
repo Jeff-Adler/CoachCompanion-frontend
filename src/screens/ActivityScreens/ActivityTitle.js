495844fd7648e6187ddc1db5e24501165cff3cc4
Jeff-Adler <Jeffm.adler@gmail.com>
2020-11-24 19:44:38 -0500

    state in activity title

--- src/screens/ActivityScreens/ActivityTitle.js
+++ src/screens/ActivityScreens/ActivityTitle.js
@@ -3,23 +3,16 @@ import { View, StyleSheet } from "react-native";
 import { Input, Button } from "react-native-elements";
 
 class ActivityTitle extends React.Component {
-  state = {
-    activity : null
-  };
-
-//   onChangeText = (name) => (text) => this.setState({ [name]: text });
-
   render() {
-    // const { username, password } = this.state;
-    const { activityTitle} = this.props;
+    const { onChangeText, activityTitle } = this.props;
     return (
       <View style={styles.container}>
         <Input
           placeholder="Username"
-          onChangeText={this.onChangeText("activityTitle")}
+          onChangeText={onChangeText("activityTitle")}
           value={activityTitle}
         />
-        {/* <Button title="Submit" onPress={() => loginHandler(this.state)} /> */}
+        <Button title="Submit" onPress={() => navigation.navigate("PointValue")} /> 
       </View>
     );
   }
