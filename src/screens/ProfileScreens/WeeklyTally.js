a9ffa37ceb75f02f57e07ad1a94f0c85c85a7091
Jeff-Adler <Jeffm.adler@gmail.com>
2020-12-07 13:09:16 -0500

    formatting

--- src/screens/ProfileScreens/WeeklyTally.js
+++ src/screens/ProfileScreens/WeeklyTally.js
@@ -8,11 +8,11 @@ class WeeklyTally extends React.Component {
         const { weeklyTally } = this.props;
         return (
             <View style = {styles.container}>
-                <Text>Weekly Tally: {weeklyTally.total_points}</Text>
-                <Text>Vocation: {weeklyTally.vocation}</Text>
-                <Text>Relationships: {weeklyTally.relationship}</Text>
-                <Text>Social: {weeklyTally.social}</Text>
-                <Text>Health: {weeklyTally.health}</Text>
+                <Text>Weekly Tally: {weeklyTally.total_points}{"\n"}</Text>
+                <Text>Vocation: {weeklyTally.vocation}{"\n"}</Text>
+                <Text>Relationships: {weeklyTally.relationship}{"\n"}</Text>
+                <Text>Social: {weeklyTally.social}{"\n"}</Text>
+                <Text>Health: {weeklyTally.health}{"\n"}</Text>
             </View>
         )
     }
