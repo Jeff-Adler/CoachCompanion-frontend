7a6fe0d2606c417cc47d9d4ab39cbfe9101d0a48
Jeff-Adler <Jeffm.adler@gmail.com>
2020-12-16 13:34:39 -0500

    swapped functional component for coach container

--- src/containers/CoachContainer.js
+++ src/containers/CoachContainer.js
@@ -2,20 +2,13 @@ import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 
 
-//Adjust placehodler to allow for sync
-class CoachContainer extends React.Component {
-    // state = {
-
-    // }
-
-    render() {
-        // const {   } = this.state 
-        return (
-            <View style={styles.container}>
-                <Text>(Placeholder)</Text>
-            </View>
-        )
-    }
+//Adjust placeholder to allow for sync
+function CoachContainer () {
+    return (
+        <View style={styles.container}>
+            <Text>(Placeholder)</Text>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
